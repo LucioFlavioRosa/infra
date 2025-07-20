b4fac52048f83b0d059064bd28f2b60aeb595e4f
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-07-19 22:20:06 -0300

    Update app_service.tf

--- app_service.tf
+++ app_service.tf
@@ -15,7 +15,7 @@ resource "azurerm_app_service" "frontend_app" {
   app_service_plan_id = azurerm_service_plan.app_plan.id
 
   site_config {
-    # Erro de Segurança 8: FTP está habilitado, o que é um protocolo inseguro.
+    
     ftps_state = "FtpsOnly"
   }
 }
@@ -27,13 +27,13 @@ resource "azurerm_app_service" "backend_app" {
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_service_plan.app_plan.id
 
-  # Erro de Design 9: Ausência de configuração de VNet integration para comunicação privada.
+  
   app_settings = {
     "WEBSITE_VNET_ROUTE_ALL" = "1"
     "GEMINI_API_KEY"         = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.gemini_api_key.id})"
   }
 
-  # Erro de Segurança 10: HTTPS Only está desabilitado, permitindo tráfego HTTP.
+  
   https_only = false
 }
 
@@ -44,7 +44,7 @@ resource "azurerm_app_service" "mcp_app" {
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_service_plan.app_plan.id
 
-  # Erro de Segurança 11: CORS (Cross-Origin Resource Sharing) configurado para permitir qualquer origem.
+  
   site_config {
     cors {
       allowed_origins = ["*"]

b7591e1e0ea6e89cf977d9d45b0014f492e09a2a
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-07-19 22:20:55 -0300

    Update banco_databricks.tf

--- banco_databricks.tf
+++ banco_databricks.tf
@@ -1,5 +1,5 @@
 # Cria o servidor de Banco de Dados PostgreSQL.
-# Erro de Segurança 14: SSL não é forçado, permitindo conexões não criptografadas.
+
 resource "azurerm_postgresql_server" "postgres" {
   name                         = "psql-vuln-graphdb-prod"
   location                     = azurerm_resource_group.rg.location
@@ -9,13 +9,13 @@ resource "azurerm_postgresql_server" "postgres" {
   version                      = "11"
   administrator_login          = "psqladmin"
 
-  # Erro de Segurança 15: Senha do administrador hardcoded.
+  
   administrator_login_password = "SuperVulnerablePassword123!"
 
   ssl_enforcement_enabled = false
 }
 
-# Erro de Segurança 16: Regra de firewall do banco de dados permite acesso de qualquer IP do Azure.
+
 resource "azurerm_postgresql_firewall_rule" "allow_azure" {
   name                = "AllowAllWindowsAzureIps"
   resource_group_name = azurerm_resource_group.rg.name
@@ -31,6 +31,6 @@ resource "azurerm_databricks_workspace" "databricks" {
   location            = azurerm_resource_group.rg.location
   sku                 = "standard"
 
-  # Erro de Design/Segurança 17: Workspace do Databricks sem injeção de VNet,
+  
   # o que o expõe à rede pública por padrão.
 }
